e0ab057c44b9a19b0c77c890f3f2670a9ba8ec5a
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-29 04:35:32 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,11 +1,11 @@
 # Code your instances here
 class GardenGnome
 
-  attr_accessor :age :gluten_allergy 
+  attr_accessor :name :age :gluten_allergy
   attr_reader :name
 
-  def initialize(name)
-    @name = name
+  def initialize(personality = "evil")
+    @personality = personality
   end
 
 
