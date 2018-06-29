22742113f8f818a61c15d79f309ffc4d448d3545
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-29 04:43:06 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -10,16 +10,16 @@ class GardenGnome
   end
 
   def gnaw
-    puts "Gnawing on a tree!!!"
+    # puts "Gnawing on a tree!!!"
   end
-# 
+#
 #   def shout
 #     puts "GNARLY!!!"
 #   end
-# 
+#
 #   def introduce_self
 #     puts "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
 #   end
 # end
-# 
+#
 # gnome1 = GardenGnome.new
